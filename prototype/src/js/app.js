0c964663c3ca722fa75e3c3d55a262572d7deb19
Jakub Pabis <pabis91@gmail.com>
2018-08-07 07:06:26 +0200

    init

---
+++ prototype/src/js/app.js
@@ -0,0 +1,49 @@
+'use strict';
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function checkCookieMessage()
+{
+	if(getCookie('cookieConfirm') !== 'yes') {
+		document.getElementById('cookieMessage').classList.add('show');
+	}
+}
+
+function cookieAgree()
+{
+	setCookie('cookieConfirm', 'yes', 365);
+	document.getElementById('cookieMessage').classList.remove('show');
+}
+
+function hasClass(el, cls) 
+{
+	return el.className && new RegExp("(\\s|^)" + cls + "(\\s|$)").test(el.className);
+}
+
+function slideTo(el)
+{
+	$('html, body').animate({
+		scrollTop: $(el).offset().top
+	}, 500);
+}
